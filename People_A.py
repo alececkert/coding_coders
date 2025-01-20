b23358310c4f59346dd8bc3a47ef5bfc993e4c0c
zoyajumean <zoyajimean05@gmail.com>
2025-01-20 16:15:13 +0100

    get universities

--- People_A.py
+++ People_A.py
@@ -5,3 +5,14 @@ with open("People/A_people.json", "r") as file:
     data_A = json.load(file)
 
 
+print(data_A)
+
+
+almaMaterlabels = [record.get("ontology/almaMater_label", None) for record in data_A]
+
+# Filter out None values if needed
+almaMaterlabels = [label for label in almaMaterlabels if label is not None]
+
+# Print the results
+print(almaMaterlabels)
+
