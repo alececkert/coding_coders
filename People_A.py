b2322a9e48dd57b02189a4ddf3d8f362c45c60ef
zoyajumean <zoyajimean05@gmail.com>
2025-01-20 16:22:55 +0100

    added the names of people that went to uni

--- People_A.py
+++ People_A.py
@@ -5,7 +5,7 @@ with open("People/A_people.json", "r") as file:
     data_A = json.load(file)
 
 
-print(data_A)
+
 
 
 almaMaterlabels = [record.get("ontology/almaMater_label", None) for record in data_A]
@@ -13,6 +13,19 @@ almaMaterlabels = [record.get("ontology/almaMater_label", None) for record in da
 # Filter out None values if needed
 almaMaterlabels = [label for label in almaMaterlabels if label is not None]
 
-# Print the results
-print(almaMaterlabels)
 
+#print(almaMaterlabels)
+
+Name_and_Uni = [
+    {
+        "name": record.get("http://www.w3.org/2000/01/rdf-schema#label", None),
+        "almaMater": record.get("ontology/almaMater_label", None),
+    }
+    for record in data_A
+]
+
+filtered_Unis = [entry for entry in Name_and_Uni if entry["name"] and entry["almaMater"]]
+
+
+for entry in filtered_Unis:
+    print(f"Name: {entry['name']}, University: {entry['almaMater']}")
