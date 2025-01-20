c6bbb3aa9f87c4c1f4ac180a0b3d87fe32b5d9c1
martarutkowski <rutkowski.marta2004@gmail.com>
2025-01-20 16:17:13 +0100

    remove print

--- People_A.py
+++ People_A.py
@@ -4,10 +4,6 @@ import json
 with open("People/A_people.json", "r") as file:
     data_A = json.load(file)
 
-
-print(data_A)
-
-
 almaMaterlabels = [record.get("ontology/almaMater_label", None) for record in data_A]
 
 # Filter out None values if needed
