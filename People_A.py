4148e9b20eae3235c7d2fd8a7160eefcf01fdcc0
Alec Eckert <alececkert504@gmail.com>
2025-01-20 16:06:22 +0100

    py

--- People_A.py
+++ People_A.py
@@ -1,7 +1,7 @@
 
 import json
 
-with open("C:/Users/LENOVO/LAB/GROUP PROJECT/A_people.json", 'r') as file:
+with open("People/A_people.json", "r") as file:
     data_A = json.load(file)
 
-# Use the loaded data
+
