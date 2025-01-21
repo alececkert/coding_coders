9923ae309aba6c920c30227356ae08c6f56876df
Daniella M <daniellamiresvaldez@gmail.com>
2025-01-21 16:43:28 +0100

    figuring out bar

--- BarPlot.R
+++ BarPlot.R
@@ -14,21 +14,26 @@ data_gr_per_ivy<- datafile|>
 print(data_gr_per_ivy)
 
 barplot1 <- ggplot(data= data_gr_per_ivy)+aes(x=almaMater,y=avg_networth, color=almaMater)+
-  geom_col(fill=color)+
+  geom_col()+
   labs(
     title='Average Net Worth of Ivy League Alumni on Wikipedia',
     x= "Ivy League",
     y= "Average Net Worth in USD"
   )
 print(barplot1)
+#ggsave('barplot1.png')
+
 
 ##BAR 2: all ivy leagues into one ######
-#data_grouped_all <- datafile |> 
- # mutate()
-  #summarise(avg_netw = mean(networth))
 
-#barplot2 <- ggplot(data= data_grouped_all)+
-  aes(x=almaMater , y=avg_netw)+
+datacombined <- read_csv('combined.csv')
+data_divided<- datacombined |> 
+  summarise(meanivy_league == True) |> 
+  summarise(avg_netw = mean(networth))
+
+
+barplot2 <- ggplot(data= data_grouped_all)+
+  aes(x= data_ivy, data_nonivy, y=average_networth)+
   geom_col()+
   labs(
     title='Average Net Worth of Ivy League Alumni on Wikipedia',
