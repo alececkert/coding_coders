b1f8647e99c84f682daed3a4bd214ccabaadf652
Alec Eckert <alececkert504@gmail.com>
2025-01-21 16:03:32 +0100

    final data for now

--- People_A.py
+++ People_A.py
@@ -17,7 +17,6 @@ ivy_league_universities = [
 university_keys = [
     "ontology/almaMater_label",
     "ontology/education_label",
-    "ontology/university_label"
     
 ]
 
@@ -31,77 +30,77 @@ for file_name in letter:
     with open(f'People/{file_name}_people.json', 'r') as file:
         data = json.load(file)
 
-    # create a list with all univeristies
-    Universities = []
-    for label in data:
-        for university_key in university_keys:
-            university = label.get(university_key)
-            if university is not None:
-                if isinstance(university, str) and university.startswith('[') and university.endswith(']'):
-                    university = json.loads(university)
-                if isinstance(university, list):
-                    Universities.extend(university)
-        
-
     #print(filtered_almaMater)
 
     # select data with name, almaMater, and networth all together
-    Name_Uni_Networth = [
-        {
-            "name": label.get("http://www.w3.org/2000/01/rdf-schema#label"),
-            "almaMater": label.get(university_keys[0]),
-            "networth": label.get("ontology/networth")
-        }
-        for label in data
-    ]
+    # Name_Uni_Networth = [
+    #     {
+    #         "name": label.get("http://www.w3.org/2000/01/rdf-schema#label"),
+    #         "almaMater": label.get("ontology/almaMater_label"),
+    #         "networth": label.get("ontology/networth")
+    #     }
+    #     for label in data
+    # ]
 
     # create a list with name, uni, and networth
     filtered_data = []
-    for data in Name_Uni_Networth:
-        if data["name"] and data["almaMater"] and data["networth"]:
-            filtered_data.append(data)
+    for person in data:
+        if "http://www.w3.org/2000/01/rdf-schema#label" in person and \
+           ("ontology/almaMater_label" in person or "ontology/education_label" in person) and \
+           "ontology/networth" in person:
+            education = []
+            if "ontology/almaMater_label" in person:
+                if isinstance(person["ontology/almaMater_label"], list):
+                    education.extend(person["ontology/almaMater_label"])
+                else:
+                    education.append(person["ontology/almaMater_label"])
+            
+            if "ontology/education_label" in person:
+                if isinstance(person["ontology/education_label"], list):
+                    education.extend(person["ontology/education_label"])
+                else:
+                    education.append(person["ontology/education_label"])
+
+            filtered_data.append({
+                "name": person.get("http://www.w3.org/2000/01/rdf-schema#label"),
+                "education": education,
+                "networth": person.get("ontology/networth")
+            })
 
     #for entry in filtered_data:
      #  print (f"Name: {entry['name']}, University: {entry['almaMater']} , networth: {entry['networth']}")
 
     # filter for only people that went to an ivy league uni
     for alumnus in filtered_data:
-        Education = alumnus["almaMater"]
+        education = alumnus["education"]
         
-        
-        if isinstance(Education, list):
-          
-            if any(ivy_uni in alma for alma in Education for ivy_uni in ivy_league_universities):
-                ivy_league_alumni.append(alumnus)
-            else:
-                non_ivy_league_alumni.append(alumnus)
-        elif any(ivy_uni in Education for ivy_uni in ivy_league_universities):
-                ivy_league_alumni.append(alumnus)
+        if any(ivy_uni in alma for alma in education for ivy_uni in ivy_league_universities):
+            ivy_league_alumni.append(alumnus)
         else:
-                non_ivy_league_alumni.append(alumnus)          
+            non_ivy_league_alumni.append(alumnus)       
             
 
         
 
 
 for entry in ivy_league_alumni:
-   print(f"Name: {entry['name']}, University: {entry['almaMater']}, Networth: {entry['networth']}")
+   print(f"Name: {entry['name']}, University: {entry['education']}, Networth: {entry['networth']}")
 
 for entry in non_ivy_league_alumni:
-    print(f"Name: {entry['name']}, University: {entry['almaMater']}, Networth: {entry['networth']}")
+    print(f"Name: {entry['name']}, University: {entry['education']}, Networth: {entry['networth']}")
 
 with open("ivy_league.csv", "w", encoding="utf-8", newline="") as file:
-    writer = csv.DictWriter(file, ["name", "almaMater", "networth"])
+    writer = csv.DictWriter(file, ["name", "education", "networth"])
     writer.writeheader()
     writer.writerows(ivy_league_alumni)
 
 with open("non_ivy_league.csv", "w", encoding="utf-8", newline="") as file:
-    writer = csv.DictWriter(file, ["name", "almaMater", "networth"])
+    writer = csv.DictWriter(file, ["name", "education", "networth"])
     writer.writeheader()
     writer.writerows(non_ivy_league_alumni)
 
 with open("combined.csv", "w", encoding="utf-8", newline="") as file:
-    writer = csv.DictWriter(file, ["name", "almaMater", "networth"])
+    writer = csv.DictWriter(file, ["name", "education", "networth"])
     writer.writeheader()
     writer.writerows(ivy_league_alumni)
     writer.writerows(non_ivy_league_alumni)
