2885728783bc0a05471281523c5f52d66cf038a2
Alec Eckert <alececkert504@gmail.com>
2025-01-24 16:13:09 +0100

    with proper y axis

--- ourfinalproject.R
+++ ourfinalproject.R
@@ -17,9 +17,10 @@ data_gr_per_ivy<- datafile|>
 print(data_gr_per_ivy)
 
 
-barplot1 <- ggplot(data= data_gr_per_ivy)+aes(x=almaMater,y=avg_networth)+
+barplot1 <- ggplot(data= data_gr_per_ivy)+aes(x=almaMater,y=(avg_networth/1000000))+
   geom_col()+
   xlab(NULL)+
+  scale_y_continuous(labels = scales::dollar_format(prefix="$", suffix = "M"))+
   labs(
     color= 'Ivy League School',
     title='Average Net Worth of Ivy League Alumni on Wikipedia',
@@ -50,28 +51,29 @@ data_divided_mean<- data_divided |>
 print(data_divided)
 
 barplot2 <- ggplot(data= data_divided_mean)+
-  aes(x= ivy_league, y=networth, fill= ivy_league)+
+  aes(x= ivy_league, y=(networth/1000000), fill= ivy_league)+
   geom_col()+
+  scale_y_continuous(labels = scales::dollar_format(prefix="$", suffix = "M"))+
   labs(
     x= "Ivy League Alumni",
     y= "Average Net Worth in USD",
     fill = 'Education'
   )+
-  theme_clean(base_size = 18)
+  theme_clean(base_size = 16)
     
 print(barplot2)
 ggsave('barplot.pdf')
 
 violinplot <- ggplot(data= data_divided)+
-  aes(x= ivy_league, y=networth, fill= ivy_league)+
+  aes(x= ivy_league, y=(networth/1000000), fill= ivy_league)+
   geom_violin()+
   labs(
     x= "Ivy League Alumni",
     y= "Average Net Worth in USD",
     fill = 'Education'
   )+
-  scale_y_log10()+
-  theme_clean(base_size = 18)
+  scale_y_log10(labels = scales::dollar_format(prefix="$", suffix = "M"))+
+  theme_clean(base_size = 16)
 
 print(violinplot)
 ggsave('violin.pdf')
@@ -86,62 +88,22 @@ datawithyears <- read_csv('not_rich_with_birth_year_after_1920.csv')|>
     ivy_league = ifelse(ivy_league, "Ivy league", "Non-Ivy league"))
 
 averages <- datawithyears|>
+  
   group_by(birthYear, ivy_league)|>
   summarise(avg_networth = mean(networth,na.rm= TRUE))|>
   ungroup()
 
 barplot3 <- ggplot(data= datawithyears)+
-  aes(x=birthYear, y=networth, color=ivy_league)+
+  aes(x=birthYear, y=(networth/1000000), color=ivy_league)+
   geom_point(size = 0.5)+
-  geom_line(data = averages, aes(x= birthYear, y= avg_networth, color= ivy_league), size=1)+
-  scale_y_log10()+
+  geom_line(data = averages, aes(x= birthYear, y= avg_networth/1000000, color= ivy_league), size=1)+
+  scale_y_log10(labels = scales::dollar_format(prefix="$", suffix = "M"))+
   labs(
     x= "Year of Birth",
     y= "Net Worth in USD", 
     color = 'Education'
   )+
-  theme_clean(base_size =18 )
+  theme_clean(base_size =16 )
 
 print(barplot3)
 ggsave('scatterlineplot.pdf')
-
-
-#####OVER 5 YEARS??
-datawith5years <- read_csv('not_rich_with_birth_year_after_1920.csv') |>
-  mutate(
-    networth = as.numeric(networth),
-    birthYear = as.numeric(birthYear),
-    ivy_league = ifelse(ivy_league, "Ivy league", "Non-Ivy league")
-  )
-
-datawith5years <- datawithyears |>
-  mutate(
-    birthYearGroup = (birthYear %/% 5) * 5
-  )
-
-
-#1987 %/% 5 = 397.4 *5 = 1985
-#1988 %/% 5 = 397.6 *5= 1985
-#1990 %/% 5 = 398 *5 = 1990
-#1991 %/% 5 = 398.2 *5 = 1990
-
-
-averages_5yr <- datawith5years |>
-  group_by(birthYearGroup, ivy_league) |>
-  summarise(avg_networth = mean(networth, na.rm = TRUE)) |>
-  ungroup()
-
-scatterplot5yrinterval <- ggplot(data = datawith5years) +
-  aes(x = birthYearGroup, y = networth, color = ivy_league) +
-  geom_point(size = 0.5)+
-  geom_line(data = averages_5yr, aes(x = birthYearGroup, y = avg_networth, color = ivy_league), size = 1) +
-  scale_y_log10() +
-  labs(
-    x = "Birth Year (Grouped by 5-Year Intervals)",
-    y = "Net Worth in USD",
-    color = "Education"
-  ) +
-  theme_clean()
-
-print(scatterplot5yrinterval)
-ggsave('scatterlineplot_5yr.pdf')
